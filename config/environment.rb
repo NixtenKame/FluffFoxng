591e7033b995e704838f9eb5852aa4f57a983a52
NixtenKame <vincenttroesch19@gmail.com>
2026-03-05 14:28:45 -0600

    [env] What is that for? someone explain lol

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@
 # Load the Rails application.
 require_relative "application"
 
+# What is this for? Is this part of production environment? I used .env.production and I wonder if I was supposed to use .env.local for production XD
 Dotenv.load(Rails.root + ".env.local")
 
 # Initialize the Rails application.
